9150629cc1f1117cbf4ccb8cdb7a5d17da890e8d
Smulemun <pokemon88005553535@gmail.com>
2023-11-03 17:39:24 +0300

    added visualization

---
+++ src/visualization/visualize.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+METRICS = ['SIM', 'ACC', 'FLNC', 'J']
+METRIC2NAME = {'SIM': 'Semantic Similarity', 'ACC': 'Style Accuracy', 'FLNC': 'Fluency', 'J': 'J Score'}
+
+def plot_metrics(data):
+    
+    # for each metric plotting a bar chart
+    for i, metric in enumerate(METRICS):
+        bars = []
+        for key in data.keys():
+            bars.append(data[key][metric])
+        plt.figure(figsize=(10, 8))
+        plt.bar(data.keys(), bars)
+        plt.title(METRIC2NAME[metric])
+        plt.savefig(f'../reports/figures/{METRIC2NAME[metric].lower().replace(" ", "_")}.png')
+        plt.show()
